62a9ded29eb62ab51ae12c0f26c15e0cb58f2c50
Edwin Melara Landaverde <melara0606@gmail.com>
2020-08-07 00:54:12 -0600

    Page second

--- lib/src/page/Scroll/widgets/page_second.dart
+++ lib/src/page/Scroll/widgets/page_second.dart
@@ -4,8 +4,23 @@ class PageSecond extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1),
       child: Center(
-        child: Text('second page'),
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          textColor: Colors.white,
+          color: Colors.blueAccent,
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text(
+              'Bienvenidos',
+              style: TextStyle(fontSize: 18.0, color: Colors.white),
+            ),
+          ),
+        ),
       ),
     );
   }
