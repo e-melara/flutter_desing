4df7d8a8b53e6ba1a27d78b42670c36c1edb654f
Edwin Melara Landaverde <melara0606@gmail.com>
2020-08-06 23:31:29 -0600

    Titulo header

--- lib/src/page/Basico/basico_page.dart
+++ lib/src/page/Basico/basico_page.dart
@@ -6,6 +6,28 @@ class BasicoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textWidget = Container(
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Lago con un puente', style: styleTitle),
+                SizedBox(height: 7.0),
+                Text(
+                  'Un lago que se encuentra en Alemania',
+                  style: styleSubTitle,
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.star, size: 30.0, color: Colors.red),
+          Text('41', style: TextStyle(fontSize: 20.0)),
+        ],
+      ),
+    );
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -15,6 +37,7 @@ class BasicoPage extends StatelessWidget {
               'assets/img/image.jpg',
             ),
           ),
+          textWidget,
         ],
       ),
     );
