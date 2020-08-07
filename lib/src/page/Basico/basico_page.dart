777f5bafcba2ae341649d41fd4bcbf9dec0b2f36
Edwin Melara Landaverde <melara0606@gmail.com>
2020-08-07 00:01:17 -0600

    Arreglos a basico page

--- lib/src/page/Basico/basico_page.dart
+++ lib/src/page/Basico/basico_page.dart
@@ -8,26 +8,39 @@ class BasicoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textWidget = Container(
-      padding: EdgeInsets.all(10.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Lago con un puente', style: styleTitle),
-                SizedBox(height: 7.0),
-                Text(
-                  'Un lago que se encuentra en Alemania',
-                  style: styleSubTitle,
-                )
-              ],
+    final textWidget = SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: styleTitle),
+                  SizedBox(height: 7.0),
+                  Text(
+                    'Un lago que se encuentra en Alemania',
+                    style: styleSubTitle,
+                  )
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, size: 30.0, color: Colors.red),
-          Text('41', style: TextStyle(fontSize: 20.0)),
-        ],
+            Icon(Icons.star, size: 30.0, color: Colors.red),
+            Text('41', style: TextStyle(fontSize: 20.0)),
+          ],
+        ),
+      ),
+    );
+
+    final textDescription = SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        child: Text(
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quibusdam reprehenderit quia iste tempora atque aspernatur exercitationem saepe nam? Minima est suscipit reprehenderit placeat aut et quae nihil? Libero, neque.\n\nLorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quibusdam reprehenderit quia iste tempora atque aspernatur exercitationem saepe nam? Minima est suscipit reprehenderit placeat aut et quae nihil? Libero, neque.",
+          textAlign: TextAlign.justify,
+          style: TextStyle(fontSize: 15.0),
+        ),
       ),
     );
 
@@ -41,18 +54,30 @@ class BasicoPage extends StatelessWidget {
     );
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Image(
-            fit: BoxFit.cover,
-            image: AssetImage(
-              'assets/img/image.jpg',
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Image(
+                height: 200.0,
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/img/image.jpg',
+                ),
+              ),
             ),
-          ),
-          textWidget,
-          SizedBox(height: 10.0),
-          acciones,
-        ],
+            textWidget,
+            SizedBox(height: 10.0),
+            acciones,
+            SizedBox(height: 30.0),
+            textDescription,
+            textDescription,
+            textDescription,
+            textDescription,
+            textDescription,
+          ],
+        ),
       ),
     );
   }
